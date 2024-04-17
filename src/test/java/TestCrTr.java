7512d4cb6be4a542a5e7aa4149da87abfab0b56e
SerzhSeryozhka <Jk3103KS@yandex.ru>
2024-04-17 17:19:27 +0300

    1

--- src/test/java/TestCrTr.java
+++ src/test/java/TestCrTr.java
@@ -1,8 +1,18 @@
+import KSA.Suit;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static KSA.Suit.Diamonds;
+import static KSA.Suit.Hearts;
 
 public class TestCrTr {
     @Test
     public void testReading(){
-
+        ArrayList<Suit> playingCards=new ArrayList<>();
+        playingCards.add(6, Diamonds);
+        playingCards.add(7, Hearts);
+        playingCards.add(8, Suit.Spades);
+//        playingCards.add()
     }
 }
